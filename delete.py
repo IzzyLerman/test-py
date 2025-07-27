05368dd8b612b59e7bd32d2e9e863dc0a5a9b845
Nolan Cutler <nolanbailey21@gmail.com>
2025-07-26 23:20:58 -0700

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,6 +1,7 @@
 def print_and_delete(folder, filename):
-    faasr_get_file(local_file=filename, remote_file=filename, remote_folder=folder)
-    with open(f"/tmp/{filename}", 'r') as f:
+    tmp_dir.mkdir(parents=True, exists_ok=True)
+    faasr_get_file(local_file=filename, local_folder="/tmp", remote_file=filename, remote_folder=folder)
+    with open(f"{filename}", 'r') as f:
         print(f"downloaded content: {f.readline()}")
     faasr_delete_file(remote_file=filename, remote_folder=folder)
     print("file deleted -- returning True")
